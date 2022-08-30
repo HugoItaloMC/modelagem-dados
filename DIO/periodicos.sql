839ac79cf21a004b824a0318a2059fd92d766c1b
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2022-08-29 23:37:25 -0300

    update my learning DataBaseExperience

--- DIO/periodicos.sql
+++ DIO/periodicos.sql
@@ -7,18 +7,16 @@ CREATE TABLE periodicos(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     nome varchar(120),
     issn INTEGER,
-    id_editora INT
+    id_editora INT,
 );
 
 CREATE TABLE editora(
     id INTEGER,
     nome_editora VARCHAR(120) UNIQUE,
     Pais INTEGER,
+    id_autor INT
     PRIMARY KEY (id)
 );
-
-/* Criando link entre as entidades aplicando constraint(restricao) */
-ALTER TABLE periodicos ADD CONSTRAINT fk_periodicos_editora FOREIGN KEY(id_editora) REFERENCES editora(id);
 
 /* Adicionando Dados em uma entidade : */
 INSERT INTO editora (nome_editora, pais) VALUES ('IEEE', 'EUA'), ('DataScienceAcademy', 'EUA');
@@ -31,4 +29,20 @@ INSERT INTO periodicos(nome_periodicos, issn, id_editora) VALUES ('Special Issue
        conceitual de ER (fluxo-grama).
 */
 
+CREATE TABLE autor(
+    id INTEGER PRIMARY KEY,
+    nome_autor VARCHAR(120),
+    id_pesquisa INTEGER,
+);
 
+CREATE TABLE pesquisa(
+    id INTEGER PRIMARY KEY,
+    nome VARCHAR(120),
+    inquiry VARCHAR(200) /*  Descrevendo pesquisa */
+)
+
+/* Criando link'S entre as entidades aplicando constraint(restricao) */
+
+ALTER TABLE periodicos ADD CONSTRAINT fk_periodicos_editora FOREIGN KEY(id_editora) REFERENCES editora(id);
+ALTER TABLE editora ADD CONSTRAINT fk_editora_autor FOREIGN KEY(id_autor) REFERENCES autor(id);
+ALTER TABLE autor ADD CONSTRAINT fk_autor_pesquisa FOREIGN KEY(id_pesquisa) REFERENCES pesquisa(id);
